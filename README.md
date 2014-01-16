93efb0ff48732da6e84ccb45e86d05f9a4bac85f
yacaFx <sergiobritor@gmail.com>
2014-01-16 03:23:48 -0600

    Upload all final code proyect

--- README.md
+++ README.md
@@ -2,3 +2,12 @@ Android_NoBackBar_Sample_BB10
 =============================
 
 Aplicación Android portada a BlackBerry 10 sin Action Bar / Back Bar
+
+El tutorial y explicacion de el código usado en este repositorio podrán encontrarlo en: <a href="http://wp.me/p3UsEt-vX">http://wp.me/p3UsEt-vX</a>
+
+<ul>
+	<li>El archivo APK final a empaquetar podrán encontrarlo en <em>SinBackBar/app</em></li>
+	<li>El archivo de configuración y el archivo bar generado podrán encontrarlo en <em>bb/</em></li>
+</ul>
+
+Cualquier duda o pregunta pueden contactarme en twitter <a href="http://www.twitter.com/yacafx">@yacaFx</a> o en <a href="mailto:yacafx@riactive.mx">yacafx@riactive.mx</a>
---
+++ SinBackBar/app/src/main/java/com/riactive/app/MainActivity.java
@@ -0,0 +1,77 @@
+package com.riactive.app;
+
+import android.app.Activity;
+import android.app.ActionBar;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+    private Button btnSaludar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_main);
+
+
+
+        btnSaludar = (Button) findViewById(R.id.btnSaludar);
+        btnSaludar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast mensaje = Toast.makeText(getApplicationContext(), "Hola Mundo!!!", Toast.LENGTH_SHORT);
+                mensaje.show();
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+            return rootView;
+        }
+
+
+    }
+
+}
